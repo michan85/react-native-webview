53c8ba8e49c3ad00a26dc0e7581c558168cc53f0
michael hancock <michael.hancock@24.com>
2020-04-09 16:36:07 +0200

    fix(1297):  backport to 7.6.0

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -1157,3 +1157,18 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     }
   }
 }
+    WebChromeClient mWebChromeClient;
+    @Override
+    public void setWebChromeClient(WebChromeClient client) {
+      this.mWebChromeClient = client;
+      super.setWebChromeClient(client);
+    }
+
+    @Override
+    public void destroy() {
+      if(mWebChromeClient!=null){
+        mWebChromeClient.onHideCustomView();
+      }
+      super.destroy();
+    }
+
